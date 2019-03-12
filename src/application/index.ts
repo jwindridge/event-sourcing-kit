69b12daa132d2c2ac2a43a67089217d1770cedbb
jonathan.windridge <jonathan.windridge@trium-capital.com>
2019-03-12 17:35:34 +0000

    Add ApplicationCommand creation test

--- src/application/index.ts
+++ src/application/index.ts
@@ -2,3 +2,4 @@ export * from './interfaces';
 
 export { createCommand, IApplicationCommand } from './Command';
 export { createEvent, IApplicationEvent } from './Event';
+export { commandHandlerFactory } from './CommandHandler'
