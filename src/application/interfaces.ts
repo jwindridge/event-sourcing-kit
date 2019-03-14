83a2e26b44db5737af21383bf66d27a69f2b05ff
Jonathan Windridge <jonathan.windridge@gmail.com>
2019-03-14 21:30:15 +0000

    Remove separate `Event` file

--- src/application/interfaces.ts
+++ src/application/interfaces.ts
@@ -1,4 +1,17 @@
+import { IDomainEvent } from '../domain';
+
 export interface IAggregateId {
   name: string;
   id: string;
+}
+
+export interface IApplicationEvent extends IDomainEvent {
+  // Unique identifier for this event
+  id: number;
+
+  // Aggregate that emitted this event
+  aggregate: IAggregateId;
+
+  // Version of the aggregate at the time the event was emitted
+  version: number;
 }
