68af9985c65f64e36e75dbe2f2fbe2bf096fa75d
Jonathan Windridge <jonathan.windridge@gmail.com>
2019-05-15 16:17:04 +0100

    fix: Export command functions from package root

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,4 +2,5 @@ export * from './errors';
 export * from './interfaces';
 
 export { createAggregateRoot } from './AggregateRoot';
+export { createCommand, createCommandValidator } from './Command';
 export { createAggregateEvent, createEvent } from './Event';
