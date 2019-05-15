47ccab7436e1de9a4d2f05d5a0a78e344f60650c
Jonathan Windridge <jonathan.windridge@gmail.com>
2019-05-15 17:43:32 +0100

    fix: Fix order of constructor arguments

--- packages/core/src/errors/UnknownCommandError.ts
+++ packages/core/src/errors/UnknownCommandError.ts
@@ -1,5 +1,6 @@
 export default class UnknownCommandError extends Error {
-  constructor(commandName: string, aggregateName: string) {
+  constructor(aggregateName: string, commandName: string) {
     super(`Unknown command '${aggregateName}.${commandName}'`);
+    this.name = 'UnknownCommandError';
   }
 }
--- packages/core/src/errors/UnknownEventError.ts
+++ packages/core/src/errors/UnknownEventError.ts
@@ -1,5 +1,6 @@
 export default class UnknownEventError extends Error {
   constructor(aggregateName: string, eventName: string) {
     super(`Unknown event type '${aggregateName}.${eventName}'`);
+    this.name = 'UnknownEventError';
   }
 }
